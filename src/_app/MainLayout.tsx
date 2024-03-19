1f2550bffdc6c1afdde9319220431c3d45a4b702
Nadir Özsoy <thisisnadirozsoyy@gmail.com>
2024-03-19 14:15:20 +0300

    Update MainLayout component to include min-height property

--- src/_app/MainLayout.tsx
+++ src/_app/MainLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 export default function MainLayout() {
   return (
-    <main className='bg-slate-200'>
+    <main className='min-h-dvh bg-slate-200'>
       <MainHeader />
       <div className='min-h-[calc(100dvh-16rem)] max-sm:min-h-[calc(100dvh-24rem)]'>
         <Outlet />
